9d24b09793aedc0be97fa12aadb111dc16792032
Raghav1378 <128809994+Raghav1378@users.noreply.github.com>
2024-02-21 12:39:52 +0530

    Add files via upload

---
+++ ra.js
@@ -0,0 +1,64 @@
+// console.log("helo this is the p=anda")
+// console.log(5+5)
+// console.log("this code is the worth to write cause im best proffesional coder and i will be one of the"+
+// " richest peroson in the world")
+// console.log(5+7)
+// let is not global scope while var is golobal scope let cannot allow two same varibale 
+// var a=5;
+// let b=4;
+// console.log(b);
+// if(true){
+//     let c=4;
+//     console.log(c);
+// }
+// var allow same name vairable while let does not 
+//  var a=54;
+//  console.log(a);
+//  const v=4;
+// //  console.log(v);
+//  var a=5,b=5,c=5;
+//  console.log(a,b,c);
+
+// let a=5;
+// a='raghav';
+// a=54;
+// console.log(a);
+//  var c={
+//     n:"panda",a:43,category:"animal"
+//  };
+// let person={
+//     name:"raghav",
+//     age:34,
+// caste:"hindu"
+// };
+// console.log(person)
+
+// var g=4;
+// console.log(g);
+// console.log(c);
+// alert("this a message to check the alert");
+// var nae=['arr','bg','fd'];
+// console.log(nae[2]);
+// console.log("hrlo");
+// alert("ahhah u got hacked");
+// var a={
+//     name:"rahj",age:43,caste:"hindu",
+// };
+// console.log(a);
+//  a=5-6;
+// //  console.log(a);
+// var a=4,b=4;
+// var ans=(++a)*(++b);
+// console.log(ans);
+
+// Comparision operator(===,!==)
+
+// console.log(45!=5);
+// differnce between ==(lose equlit value only) and ===(strict means datatypes and value same)
+// let num=1;
+// let str='1';
+// console.log(num==str);
+// console.log(num===str);
+
+
+// ternary operator
